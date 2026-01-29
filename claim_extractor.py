6f7c5803a914667abd26b5d30b70c96a2c6d68f2
mudit-mohit <muditmohitkumarsingh@gmail.com>
2026-01-29 14:45:12 +0530

    Now updated langchain updated parser

--- claim_extractor.py
+++ claim_extractor.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import PromptTemplate
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 
 
